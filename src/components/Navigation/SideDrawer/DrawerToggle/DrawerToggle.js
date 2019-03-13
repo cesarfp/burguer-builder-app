61ad40bfbcc22fc06d27131daecc23aeb612c90b
César <cesar.franco@4thsource.com>
2019-03-13 17:23:58 -0600

    class 175

--- src/components/Navigation/SideDrawer/DrawerToggle/DrawerToggle.js
+++ src/components/Navigation/SideDrawer/DrawerToggle/DrawerToggle.js
@@ -1,8 +1,11 @@
 import React from 'react'
+import classes from './DrawerToggle.css'
 
 const drawerToggle = (props) => (
-	<div onClick={props.clicked}>
-		<img src="" alt="drawerToggle"></img>
+	<div className={classes.DrawerToggle} onClick={props.clicked}>
+		<div></div>
+		<div></div>
+		<div></div>
 	</div>
 )
 
