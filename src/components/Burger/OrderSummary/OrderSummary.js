a4c27b3fcbd816d0d9783a5db22cf72e381d88bf
César <cesar.franco@4thsource.com>
2019-03-14 10:00:43 -0600

    class 178

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,30 +1,39 @@
-import React from 'react'
+import React, {Component} from 'react'
 import Auxiliary from '../../../hoc/Auxiliary'
 import Button from '../../UI/Button/Button'
 
-const orderSummary = (props) => {
-	const ingredientSummary = Object.keys(props.ingredients)
+class OrderSummary extends Component {
+	// This could be a functional component, doesn't have to be a class.
+
+	componentDidUpdate(){
+		console.log('[OrderSummary] DidUpdate')
+	}
+
+	render(){
+
+		const ingredientSummary = Object.keys(this.props.ingredients)
 		.map(igKey => {
 			return (
 				<li key={igKey}>
-					<span style={{textTransform:'capitalize'}}>{igKey}</span>: {props.ingredients[igKey]}
+					<span style={{textTransform:'capitalize'}}>{igKey}</span>: {this.props.ingredients[igKey]}
 				</li>
 			)
 		})
 
-	return (
-		<Auxiliary>
-			<h3>Your Order</h3>
-			<p>A delicious burger with the following ingredients:</p>
-			<ul>
-				{ingredientSummary}
-			</ul>
-			<p><strong>Total Price: {props.price}</strong></p>
-			<p>Continue to Checkout?</p>
-			<Button btnType="Danger" clicked={props.purchaseCancelled}>CANCEL</Button>
-			<Button btnType="Success" clicked={props.purchaseContinued}>CONTINUE</Button>
-		</Auxiliary>
-	)
-};
+		return (
+			<Auxiliary>
+				<h3>Your Order</h3>
+				<p>A delicious burger with the following ingredients:</p>
+				<ul>
+					{ingredientSummary}
+				</ul>
+				<p><strong>Total Price: {this.props.price}</strong></p>
+				<p>Continue to Checkout?</p>
+				<Button btnType="Danger" clicked={this.props.purchaseCancelled}>CANCEL</Button>
+				<Button btnType="Success" clicked={this.props.purchaseContinued}>CONTINUE</Button>
+			</Auxiliary>
+		)
+	}
+}
 
-export default orderSummary
+export default OrderSummary
