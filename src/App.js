d8d8ed1cce8769e5f68c384aa8427b97b4cd63e8
César <cesar.franco@4thsource.com>
2019-03-27 16:35:37 -0600

    class 249

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
 import Layout from './hoc/Layout/Layout'
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder'
-import Checkout from "./containers/Checkout/Checkout";
+import Checkout from './containers/Checkout/Checkout'
+import { Route } from "react-router-dom";
+
 
 class App extends Component {
  
@@ -9,8 +11,8 @@ class App extends Component {
     return (
       <div >
         <Layout>
-          <BurgerBuilder/>
-          <Checkout/>
+          <Route path="/" exact component={BurgerBuilder}/>
+          <Route path="/checkout" component={Checkout}/>
         </Layout>
       </div>
     );
--- src/containers/Checkout/Checkout.js
+++ src/containers/Checkout/Checkout.js
@@ -11,9 +11,46 @@ class Checkout extends Component {
 		} 
 	}
 
+	componentDidMount(){
+		const query = new URLSearchParams(this.props.location.search)
+		const ingredients = {}
+		for(let param of query.entries()){
+			ingredients[param[0]] = +param[1]
+		}
+
+		this.setState({ingredients:ingredients})
+
+	}
+
+	checkoutCancelledHandler = () => {
+		//this.props.history.goBack();
+		// this.props.history.push('/')
+
+		const queryParams = []
+		for(let i in this.state.ingredients){
+			queryParams.push(encodeURIComponent(i) + '=' + encodeURIComponent(this.state.ingredients[i]))
+		}
+		const queryString = queryParams.join('&')
+
+		this.props.history.push({
+			pathname:'/',
+			search:'?' + queryString
+		})
+	}
+	
+	checkoutContinueHandler = () => {
+		this.props.history.replace('/checkout/contact-data');
+	}
+
 	render() { 
+		console.log(this.props)
 		return(
-			<CheckoutSummary ingredients={this.state.ingredients}/>
+			<CheckoutSummary 
+				ingredients={this.state.ingredients}
+				cancelOrder={this.cancelOrderHandler}
+				checkoutCancelled={this.checkoutCancelledHandler}
+				checkoutContinued={this.checkoutContinueHandler}
+			/>
 		)
 	}
 }
