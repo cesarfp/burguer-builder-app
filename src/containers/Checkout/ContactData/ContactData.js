c2381a6df5538f4cf8c2ea4f1766ebf7140886d2
César <cesar.franco@4thsource.com>
2019-04-01 10:13:39 -0600

    class 268

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -41,7 +41,7 @@ class ContactData extends Component {
 				value:'',
 				validation:{
 					required:true,
-					minLength: 5,
+					minLength: 3,
 					maxLength: 5
 				},
 				valid:false
@@ -120,17 +120,17 @@ class ContactData extends Component {
 	}
 
 	checkValidity(value, rules){
-		let isValid = false
+		let isValid = true
 		if(rules.required){
-			isValid = value.trim() !== ''
+			isValid = value.trim() !== '' && isValid;
 		}
 
-		if(rules.minLenght){
-			isValid = value.length >= rules.minLenght
+		if(rules.minLength){
+			isValid = value.length >= rules.minLength && isValid
 		}
 		
 		if(rules.maxLength){
-			isValid = value.length <= rules.maxLenght
+			isValid = value.length <= rules.maxLength && isValid
 		}
 
 		return isValid
@@ -173,6 +173,8 @@ class ContactData extends Component {
 							elementType={formElement.config.elementType} 
 							elementConfig={formElement.config.elementConfig}  
 							value={formElement.config.value}
+							invalid={!formElement.config.valid}
+							shouldValidate={formElement.config.validation}
 							changed={(event)=>this.inputChangedHandler(event, formElement.id)} />
 
 					))
