1e7317da3c3eb05a0ef38ddaa991c9a5af394a22
César <cesar.franco@4thsource.com>
2019-05-16 16:17:34 -0500

    class 481

--- src/hoc/Layout/Layout.js
+++ src/hoc/Layout/Layout.js
@@ -1,4 +1,4 @@
-import React, {Component} from 'react'
+import React, {useState} from 'react'
 import {connect} from 'react-redux'
 
 import Aux from '../../hoc/Auxiliary/Auxiliary'
@@ -6,39 +6,36 @@ import classes from './Layout.css'
 import Toolbar from '../../components/Navigation/Toolbar/Toolbar'
 import SideDrawer from  '../../components/Navigation/SideDrawer/SideDrawer'
 
-class Layout extends Component {
-	state = {
-		showSideDrawer: false
+const layout = (props) => {
+	const [sideDrawerIsVisible, setSideDrawerIsVisible] = useState(false)
+	
+
+	const sideDrawerClosedHandler = () => {
+		setSideDrawerIsVisible(false)
 	}
 
-	sideDrawerClosedHandler = () => {
-		this.setState({showSideDrawer:false})
+	const sideDrawerToggleHandler = () => {
+		setSideDrawerIsVisible(!sideDrawerIsVisible)
 	}
 
-	sideDrawerToggleHandler = () => {
-		this.setState((prevState)=>{
-			return {showSideDrawer:!prevState.showSideDrawer}
-		})
-	}
 
-	render (){
-		return (
-			<Aux>
-				<Toolbar 
-					drawerToggleClicked={this.sideDrawerToggleHandler}
-					isAuth={this.props.isAuthenticated}
-				/>
-				<SideDrawer 
-					open={this.state.showSideDrawer}
-					closed={this.sideDrawerClosedHandler}
-					isAuth={this.props.isAuthenticated}
-				/>
-				<main className={classes.Content}>
-					{this.props.children}
-				</main>
-			</Aux>
-		)
-	}
+	return (
+		<Aux>
+			<Toolbar 
+				drawerToggleClicked={sideDrawerToggleHandler}
+				isAuth={props.isAuthenticated}
+			/>
+			<SideDrawer 
+				open={sideDrawerIsVisible}
+				closed={sideDrawerClosedHandler}
+				isAuth={props.isAuthenticated}
+			/>
+			<main className={classes.Content}>
+				{props.children}
+			</main>
+		</Aux>
+	)
+	
 		
 }
 
@@ -47,10 +44,6 @@ const mapStateToProps = state => {
 		isAuthenticated: state.auth.token !== null
 	}
 }
-// const mapDispatchToProps = dispatch => {
-// 	return{
-// 		onAuthLogout: () => dispatch(actions.logout())
-// 	}
-// }	
 
-export default connect(mapStateToProps)(Layout)
+
+export default connect(mapStateToProps)(layout)
