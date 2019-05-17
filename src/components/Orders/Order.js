0e68fb43a2b8141ef030254bc698fc0d1a18531d
César <cesar.franco@4thsource.com>
2019-05-17 09:49:03 -0500

    class 485 fix

--- src/components/Orders/Order.js
+++ src/components/Orders/Order.js
@@ -28,7 +28,6 @@ const Order = (props) => {
 	return(
 		<div className={classes.Order}>
 			<p>Ingredients: {ingredientOutput} </p>
-			<p>Ingredients: Salad (1)</p>
 			<p>Price: <strong>USD {props.price.toFixed(2)}</strong></p>
 		</div>
 	)
